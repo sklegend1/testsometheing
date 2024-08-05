812cc953b185b51f443780dd94f62fc8582f2e68
unknown <sut.sohrab@gmail.com>
2024-08-05 05:45:50 -0700

    test4

--- components/OffChains/Referral/MainReferral.tsx
+++ components/OffChains/Referral/MainReferral.tsx
@@ -9,19 +9,19 @@ interface RefProps{
 }
 const MainReferral = (props:RefProps) => {
     return (
-        <div className=" w-[88vw]  mx-auto mt-24 text-center text-white">
+        <div className=" w-[88vw]  mx-auto mt-12 text-center text-white">
             <h1 className=" text-4xl font-bold mb-2">{props.refCount} Referrals</h1>
             <InviteButton  />
             <div className=" secondarycolor w-[88vw] aspect-[381/104] mt-8 rounded-lg content-center">LeaderBoard Banner</div>
             <div className=" ">
-                <div className=" fixed bg-[#2A2F53] w-full rounded-3xl h-[55vh] bottom-0 left-0 p-4 ">
+                <div className=" fixed bg-[#2A2F53] w-full rounded-3xl h-[55vh] bottom-0 left-0 py-4 ">
                 <div id="ReffererToggle"
             className=" mb-4 w-[85%] mt-4 grid grid-cols-2 text-gray-300 text-center py-1 secondarycolor rounded-xl mx-auto"
             >
                 <button className=" text-gray-800 bg-blue-gray-100 w-[90%] py-1 mx-auto rounded-xl font-semibold">Top 50 Referrers</button>
                 <button>More Details</button>
             </div>
-            <div className="overflow-y-scroll h-[70%] ">
+            <div className="overflow-y-scroll h-[70%] p-3 ">
             <ReferralCard friend={52}  title="Rick Novak" point={763} 
             img="/icons/referrals/person1.png" img1="/icons/referrals/nft.png" img2="/icons/referrals/ton.png"  />
             <ReferralCard friend={38}  title="Ronald Bar" point={680} 
