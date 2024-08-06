6f2c54e9d23442c382c6bc833732ed959c23774f
unknown <sut.sohrab@gmail.com>
2024-08-06 04:00:09 -0700

    test7

--- components/OffChains/Tasks/TaskDialog.tsx
+++ components/OffChains/Tasks/TaskDialog.tsx
@@ -1,20 +1,75 @@
 'use client'
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { XCircleIcon } from "@heroicons/react/24/outline";
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
 
 interface taskProps{
     title?:string
     point?:number
+    isOpen:()=>void
     img:string
     
 }
 const TaskDialog = (props:taskProps) => {
     const [isOpen,setIsOpen]=useState(true)
+    const [verifyState,setVerifyState] = useState(0)
+    const [buttonHtml,setButtonHtml] = useState(
+    <button onClick={()=>{setVerifyState(1)}}
+    className=" col-span-5 mt-3 text-white font-bold text-center h-[9vw] bg-[#9AA6F6] rounded-md  ">
+        Join
+    </button>)
+
+        useEffect(()=>{
+            switchState()
+        },[verifyState])
+
+    const switchState=()=>{
+    switch (verifyState) {
+      case 0:
+        setButtonHtml(
+        <button onClick={()=>{setVerifyState(1)}}
+         className=" col-span-5 mt-3 text-white font-bold text-center h-[9vw] bg-[#9AA6F6] rounded-md  ">
+            Join
+        </button>);
+        break;
+      case 1:
+        setButtonHtml(
+            <button onClick={()=>{setVerifyState(2)}} className=" col-span-5 mt-3 text-white font-bold text-center h-[9vw] bg-[#029E80] rounded-md  ">
+                Verify
+            </button>);
+        break;
+      case 2:
+        setButtonHtml(
+            <button className=" col-span-5 mt-3 text-white font-bold text-center h-[9vw] bg-[#029E80] rounded-md  ">
+        <div className=" animate-spin mx-auto w-6 h-6 
+        border-t-4 border-t-[#ffffff]
+        border-l-4 border-l-[#ffffff7c]
+        border-b-4 border-b-[#ffffff7c]
+        border-r-4 border-r-[#ffffff7c]
+         rounded-full "></div>
+    </button>);
+
+    setTimeout(()=>{
+        setVerifyState(3)
+    },2000)
+        break;
+
+        case 3:
+        setButtonHtml(
+            <button onClick={()=>{props.isOpen()}}
+             className=" col-span-5 mt-3 text-white font-bold text-center h-[9vw] bg-[#029E80] rounded-md  ">
+                <CheckCircleIcon className=" mx-auto w-6" />
+            </button>);
+        break;
+    }
+}
     return (
-        isOpen&&<div className="   fixed p-5 pb-[110px] left-0 bottom-[0]  w-full secondarycolor rounded-[50px] ">
+        <div className=" modal-overlay">
+        <div className=" modal animate-[popuptask_0.4s_1_forwards]  fixed p-5 pb-[110px] 
+        left-0 bottom-[-55vh]  w-full secondarycolor rounded-2xl shadow-[0px_0px_30px_5px_rgba(150,200,255,0.7)] border-[#3f55756c] border-[1px] ">
             <div className=" relative"
-            onClick={()=>setIsOpen(false)}
+            onClick={()=>{props.isOpen()}}
             ><XCircleIcon className=" absolute right-4 w-8 text-red-500"/></div>
             <div className={"  w-full bg-gradient-to-r grid grid-cols-4 from-[30%] mt-4  rounded-xl p-2 "}
             
@@ -33,10 +88,8 @@ const TaskDialog = (props:taskProps) => {
                  mx-1 shadow-[0px_0px_4px_rgba(200,200,0,0.8)] "></div>
                 {props.point} Points
             </div>
-            <button className=" col-span-5 mt-3 text-white font-bold text-center py-3 bg-[#9AA6F6] rounded-md  ">
-                
-                JOIN
-            </button>
+            {buttonHtml}
+        </div>
         </div>
         </div>
     );
--- components/OffChains/Tasks/TasksContainer.tsx
+++ components/OffChains/Tasks/TasksContainer.tsx
@@ -6,15 +6,18 @@ import TaskDialog from "./TaskDialog";
 
 const TasksContainer = () => {
     const [isDialog,setIsDialog] = useState(false)
+    const toggleDialog=()=>{
+        setIsDialog(!isDialog)
+    }
     return (
         
         <div>
             <div onClick={()=>setIsDialog(true)}>
-            <TaskCard title="Join us on Telegram" point={10} img="/icons/taskpage/telegram.jpg"  />
+            <TaskCard title="Join us on Telegram" point={10} img="/icons/taskpage/telegram.jpg"   />
             </div>
             <TaskCard title="Subscribe us on YouTube" point={20} img="/icons/taskpage/youtube.png" />
             <TaskCard title="Follow us on X" point={15} img="/icons/taskpage/xsocial.webp" />
-            {isDialog&&<TaskDialog  title="Join us on Telegram" point={10} img="/icons/taskpage/telegram.jpg"/>}
+            {isDialog&&<TaskDialog  title="Join us on Telegram" point={10} img="/icons/taskpage/telegram.jpg" isOpen={toggleDialog}/>}
         </div>
     );
 }
