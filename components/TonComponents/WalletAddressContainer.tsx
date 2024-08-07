ab34a6c9a3ad615d1d6e1899bdb6a8cc4693da4a
unknown <sut.sohrab@gmail.com>
2024-08-07 02:41:10 -0700

    wallet balance test

--- components/TonComponents/WalletAddressContainer.tsx
+++ components/TonComponents/WalletAddressContainer.tsx
@@ -8,10 +8,27 @@ import { TonConnectUIProvider,useTonConnectUI } from "@tonconnect/ui-react"
 export const WalletAddress = () => {
     const userFriendlyAddress = useTonAddress();
     const rawAddress = useTonAddress(false);
+    const briefUFA = userFriendlyAddress.slice(0,4) +"..." + userFriendlyAddress.slice(12)
     const [tonCon] = useTonConnectUI()
+    const getBalance =async () =>{
+        const res = await fetch( "https://toncenter.com/api/v2/getAddressBalance?address="+userFriendlyAddress , {
+            method:"GET",
+            
+        })
+        const data = await res.json()
+
+        if(res.status==200){
+            return data
+        }
+        else{
+            return "No Response"
+        }
+    }
+    const balance = getBalance()
+    
     return (
         userFriendlyAddress && (
-            <div className=" pt-1 text-white text-center  w-[92%] h-[18vw] mx-auto mt-5 rounded-lg grid grid-cols-6 px-2 secondarycolor">
+            <div className=" pt-1 text-white text-center  w-[92%] h-[25vw] mx-auto mt-5 rounded-lg grid grid-cols-6 px-2 secondarycolor">
             <div className=' content-center'>
             <Image 
                     src="/icons/nav/Wallet.png"
@@ -27,9 +44,9 @@ export const WalletAddress = () => {
                 onClick={()=>{tonCon.disconnect();tonCon.connector.disconnect()}}
                 >Disconnect</button>
             </div>
-            <div className='  col-span-5 text-left text-xs'>
-                <p className=' h-8 overflow-hidden '>User-friendly address: {userFriendlyAddress}</p>
-                <p className=' h-8 overflow-hidden '>Raw address: {rawAddress}</p>
+            <div className='  col-span-5 text-center pl-1 text-sm '>
+                <p className=' h-8 overflow-hidden '>User-friendly address: {briefUFA}</p>
+                <p className=' h-8 overflow-hidden font-bold '>Balance: {rawAddress}</p>
             </div>
             
         </div>
