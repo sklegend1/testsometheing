fcab50d71c19a5daa0cd1296c07c71514315baf8
unknown <sut.sohrab@gmail.com>
2024-08-13 04:39:46 -0700

    env set 2

--- components/OffChains/Challenges/TonPredict/PredMonitor.tsx
+++ components/OffChains/Challenges/TonPredict/PredMonitor.tsx
@@ -49,7 +49,7 @@ const PredMonitor = (props:PredMonitorProps) => {
             
             <div className=" absolute ml-[-60] top-[16vw] h-[33vw]  w-[60vw] ">
                 <div className=" mx-auto text-[3vw] text-center text-white mt-[5vw]"> Last Price of TON </div>
-                <div className=" mx-auto text-[5vw] font-bold text-center text-green-600 ">$ {props.price}</div>
+                <div className={" mx-auto text-[5vw] font-bold text-center"+(props.price>props.lockedPrice?" text-green-600 ":" text-red-600 ")}>$ {props.price}</div>
                 <div className=" mt-[5%] grid grid-cols-2">
                     <div className=" text-white ml-[4vw] text-[4vw] text-left">Locked Price :</div>
                     <div className=" text-white mr-[4vw] text-[4vw] font-bold text-right">${props.lockedPrice}</div>
